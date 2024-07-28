b68007dbd4fc25c4fda162fda84a10a0f85d5b9c
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2024-07-28 11:36:24 +0300

    observer added

--- src/index.js
+++ src/index.js
@@ -1,36 +1,68 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import { getFotos } from './js/api.js';
+import { ImagesApi } from './js/api.js';
 
 const searchForm = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
+const galleryGuard = document.querySelector('.quard');
 // console.log('🚀 ~ searchForm:', searchForm);
 
 searchForm.addEventListener('submit', onFormSubmit);
+const lighgtBox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
 
-// getFotos('dscfwrfwref').then(showImages).catch(console.log);
+const api = new ImagesApi();
+
+// var options = {
+//   //   root: document.querySelector('#scrollArea'),
+//   rootMargin: '700px',
+//   //   threshold: 1.0,
+// };
+// var callback = function (entries, observer) {
+//   /* Content excerpted, show below */
+// };
+const observer = new IntersectionObserver(
+  entries => {
+    const isIntersecting = entries[0].isIntersecting;
+    console.dir(isIntersecting);
+    if (!isIntersecting) return;
+    api.setNextPage();
+    api.getData().then(showImages).catch(console.log);
+  },
+  { root: null, rootMargin: '500px', threshold: 1.0 }
+);
 
 function onFormSubmit(evt) {
   evt.preventDefault();
   const query = evt.target.elements.searchQuery.value;
   const normilizedQuery = query.trim().toLowerCase();
-  console.log('🚀 ~ onFormSubmit ~ normilizeQuery:', normilizedQuery);
+  //   console.log('🚀 ~ onFormSubmit ~ normilizeQuery:', normilizedQuery);
 
   if (!normilizedQuery) return;
-
-  getFotos(normilizedQuery).then(showImages).catch(console.log);
+  observer.unobserve(galleryGuard);
+  galleryEl.innerHTML = '';
+  api.setQuery(normilizedQuery);
+  api.getData().then(showImages).catch(console.log);
+  evt.target.reset();
+  setTimeout(() => {
+    observer.observe(galleryGuard);
+  }, 1000);
 }
 
 function showImages({ total, totalHits, hits }) {
   if (!total) {
-    Notify.info(
+    Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
     return;
   }
-  console.log(hits);
+  Notify.success(`Hooray! We found ${totalHits} images.`);
 
   galleryEl.insertAdjacentHTML('beforeend', createGalleryMarkup(hits));
+  lighgtBox.refresh();
 }
 
 function createGalleryMarkup(list) {
@@ -45,22 +77,24 @@ function createGalleryMarkup(list) {
         comments,
         downloads,
       }) => `
-    <li class="photo-card" data-url="${largeImageURL}">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes ${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${downloads}</b>
-    </p>
-  </div>
+    <li class="photo-card">
+    <a href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="400px"/>
+  </a>
+  <ul class="info">
+    <li class="info-item">
+      <b><span class = "info-label">Likes</span> ${likes}</b>
+    </li>
+    <li class="info-item">
+      <b><span class = "info-label">Views</span> ${views}</b>
+    </li>
+    <li class="info-item">
+      <b><span class = "info-label">Comments</span> ${comments}</b>
+    </li>
+    <li class="info-item">
+      <b><span class = "info-label">Downloads</span> ${downloads}</b>
+    </li>
+  </ul>
 </li>
     `
     )
--- src/js/api.js
+++ src/js/api.js
@@ -1,18 +1,50 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const apiKey = '29142435-196ab0ea47673651fa34d9a29';
+// export async function getFotos(query) {
+//   const options = {
+//     params: {
+//       key: apiKey,
+//       q: query,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       safesearch: true,
+//     },
+//   };
+//   const res = await axios.get(`${BASE_URL}`, options);
+//   return res.data;
+// }
 
-export async function getFotos(query) {
-  const options = {
-    params: {
-      key: apiKey,
-      q: query,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-    },
-  };
-  const res = await axios.get(`${BASE_URL}`, options);
-  return res.data;
+export class ImagesApi {
+  constructor() {
+    this.page = 1;
+    this.perPage = 30;
+    this.query = '';
+  }
+
+  setQuery(newQuery) {
+    this.query = newQuery;
+    this.page = 1;
+  }
+
+  setNextPage() {
+    this.page += 1;
+  }
+
+  async getData() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const apiKey = '29142435-196ab0ea47673651fa34d9a29';
+    const options = {
+      params: {
+        key: apiKey,
+        q: this.query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: this.page,
+        per_page: this.perPage,
+      },
+    };
+    const res = await axios.get(`${BASE_URL}`, options);
+    return res.data;
+  }
 }
